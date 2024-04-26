5c0046650817be1bf16a1c4e9e79d7c48249e73e
Filip Dostál <dos41071@spseol.cz>
2024-04-26 15:32:07 +0200

    posouvání pozadí

--- rocnikovy_projekt_Dostál.py
+++ rocnikovy_projekt_Dostál.py
@@ -1,17 +1,46 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
-width=600
-height=300
-screen=pygame.display.set_mode((width, height))
-pygame.display.set_caption("Harry Potter Game")
-lets_continue=True
+clock = pygame.time.Clock()
+fps = 60
 
-while lets_continue:
+screen_width= 864
+screen_height= 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+#define game variables
+ground_scroll = 0
+scroll_speed = 4
+
+#load images
+bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
+
+
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    #draw background
+    screen.blit(bg, (0,0))
+
+    #draw and scroll the ground
+    screen.blit(ground_img, (ground_scroll,768))
+    ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
+
+
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            lets_continue=False
-
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
 
 pygame.quit()
