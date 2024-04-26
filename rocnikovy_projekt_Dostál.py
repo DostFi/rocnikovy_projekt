acc298107f7878bc47b1d87ae38eaa09a1849e13
Filip Dostál <dos41071@spseol.cz>
2024-04-26 16:07:51 +0200

    přeloženo do češtiny

--- rocnikovy_projekt_Dostál.py
+++ rocnikovy_projekt_Dostál.py
@@ -6,77 +6,74 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width= 864
-screen_height= 936
+šířka_obrazovky = 864
+výška_obrazovky = 936
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+obrazovka = pygame.display.set_mode((šířka_obrazovky, výška_obrazovky))
 pygame.display.set_caption('Flappy Bird')
 
-#define game variables
-ground_scroll = 0
-scroll_speed = 4
+# Definování herních proměnných
+posun_pozadí = 0
+rychlost_posunu = 4
 
-#load images
-bg = pygame.image.load('img/bg.png')
-ground_img = pygame.image.load('img/ground.png')
+# Načtení obrázků
+pozadí = pygame.image.load('img/bg.png')
+pozemek_obr = pygame.image.load('img/ground.png')
 
 
-class Bird(pygame.sprite.Sprite):
+class Pták(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.images = []
+        self.obrázky = []
         self.index = 0
-        self.counter = 0
-        for num in range(1, 4):
-            img = pygame.image.load(f'img/bird{num}.png')
-            self.images.append(img)
-        self.image = self.images[self.index]
+        self.počítadlo = 0
+        for číslo in range(1, 4):
+            obr = pygame.image.load(f'img/bird{číslo}.png')
+            self.obrázky.append(obr)
+        self.image = self.obrázky[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
     def update(self):
 
-        #handle the animation
-        self.counter += 1
-        flap_cooldown = 5
+        # Animace
+        self.počítadlo += 1
+        prodleva_machání = 5
 
-        if self.counter > flap_cooldown:
-            self.counter = 0
+        if self.počítadlo > prodleva_machání:
+            self.počítadlo = 0
             self.index += 1
-            if self.index >= len(self.images):
+            if self.index >= len(self.obrázky):
                 self.index = 0
-        self.image = self.images[self.index]
-
-bird_group = pygame.sprite.Group()
-
-flappy = Bird(100, int(screen_height / 2))
-
-bird_group.add(flappy)
+        self.image = self.obrázky[self.index]  # Update current image
 
 
+pták_skupina = pygame.sprite.Group()
 
+flappy = Pták(100, int(výška_obrazovky / 2))
 
-run = True
-while run:
+pták_skupina.add(flappy)
+
+běh = True
+while běh:
 
     clock.tick(fps)
 
-    #draw background
-    screen.blit(bg, (0,0))
+    # Kreslení pozadí
+    obrazovka.blit(pozadí, (0,0))
 
-    bird_group.draw(screen)
-    bird_group.update()
+    pták_skupina.draw(obrazovka)
+    pták_skupina.update()
 
-    #draw and scroll the ground
-    screen.blit(ground_img, (ground_scroll,768))
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
+    # Kreslení a posun pozadí
+    obrazovka.blit(pozemek_obr, (posun_pozadí,768))
+    posun_pozadí -= rychlost_posunu
+    if abs(posun_pozadí) > 35:
+        posun_pozadí = 0
 
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    for událost in pygame.event.get():
+        if událost.type == pygame.QUIT:
+            běh = False
     pygame.display.update()
 
 pygame.quit()
