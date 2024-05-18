d4c06b6dd7127866e8e8ea990c57abb20f777edd
Filip Dostál <dos41071@spseol.cz>
2024-05-18 16:08:01 +0200

    Trubky

--- rocnikovy_projekt_Dostál.py
+++ rocnikovy_projekt_Dostál.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
@@ -17,6 +18,9 @@ posun_pozadí = 0
 rychlost_posunu = 4
 létání = False
 prohra = False
+trubka_mezera = 150
+trubka_frekvence = 1500 #milisekundy
+poslední_trubka = pygame.time.get_ticks() - trubka_frekvence
 
 # Načtení obrázků
 pozadí = pygame.image.load('img/bg.png')
@@ -72,14 +76,31 @@ class Pták(pygame.sprite.Sprite):
 
 
 
-
-
+class Trubka(pygame.sprite.Sprite):
+    def __init__(self, x, y, pozice):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        #pozice 1 je z vrchu, -1 ze spodu
+        if pozice == 1:
+           self.image = pygame.transform.flip(self.image, False, True)
+           self.rect.bottomleft = [x, y - int(trubka_mezera / 2)]
+        if pozice == -1:
+            self.rect.topleft = [x, y + int(trubka_mezera / 2)]
+    def update(self):
+        self.rect.x -= rychlost_posunu
+        if self.rect.right < 0:
+            self.kill()
 
 pták_skupina = pygame.sprite.Group()
+trubka_skupina = pygame.sprite.Group()
 
 flappy = Pták(100, int(výška_obrazovky / 2))
 
 pták_skupina.add(flappy)
+
+
+
 
 pokračování = True
 while pokračování:
@@ -91,19 +112,35 @@ while pokračování:
 
     pták_skupina.draw(obrazovka)
     pták_skupina.update()
+    trubka_skupina.draw(obrazovka)
+
     #nakreslení země
     obrazovka.blit(pozadí_obrázek, (posun_pozadí,768))
 
+    #kolize
+    if pygame.sprite.groupcollide(pták_skupina, trubka_skupina, False, False) or flappy.rect.top < 0:
+        prohra = True
     # kontrola jestli pták spadnul na zem
-    if flappy.rect.bottom > 768:
+    if flappy.rect.bottom >= 768:
         prohra = True
         létání = False
 
-    if prohra == False:
+    if prohra == False and létání == True:
+
+        # generace nových trubek
+        čas_teď = pygame.time.get_ticks()
+        if čas_teď - poslední_trubka > trubka_frekvence:
+            trubka_výška = random.randint(-100, 100)
+            spodní_trubky = Trubka(šířka_obrazovky, int(výška_obrazovky / 2) + trubka_výška, -1)
+            vrchní_trubky = Trubka(šířka_obrazovky, int(výška_obrazovky / 2) + trubka_výška, 1)
+            trubka_skupina.add(spodní_trubky)
+            trubka_skupina.add(vrchní_trubky)
+            poslední_trubka = čas_teď
         # Kreslení a posun pozadí
         posun_pozadí -= rychlost_posunu
         if abs(posun_pozadí) > 35:
             posun_pozadí = 0
+        trubka_skupina.update()
 
     for událost in pygame.event.get():
         if událost.type == pygame.QUIT:
